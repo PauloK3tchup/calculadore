b0b9057fbec0ae7f5705dff9d11422be6ffb7c23
PauloK3tchup <passandoalimpo13@gmail.com>
2022-08-17 10:28:35 -0300

    Primeiro Poggit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script></script>
+<template>
+  <form>
+    <label for="preco">Valor do produto</label>
+    <input id="preco" type="number" /><br />
+    <label for="jorgeta">Selecione a qualidade do serviço</label>
+    <select id="jorgeta">
+      <option value="" disabled>Selecione</option>
+      <option value="repugnante">Repugnante (5%)</option>
+      <option value="horrendo">Horrendo (10%)</option>
+      <option value="mais ou menos">Mais ou menos (15%)</option>
+      <option value="bah">Bah (20%)</option>
+      <option value="morbastico">Morbástico (25%)</option>
+      <option value="grutastico">Grutástico (30%)</option>
+    </select>
+  </form>
+</template>
+<style></style>
